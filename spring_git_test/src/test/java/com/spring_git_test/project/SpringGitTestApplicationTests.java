83401c81da879b777f683bb057c0d7a21fe665ea
bomooline <bomooline@naver.com>
2022-12-12 10:56:15 +0900

    초기 업로드

---
+++ spring_git_test/src/test/java/com/spring_git_test/project/SpringGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_git_test.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
